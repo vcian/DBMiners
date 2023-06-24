f85dcd5b9a2cf643f74387a13a0c94db22c57f93
vagrant <mukund.parekh@viitor.cloud>
2023-06-24 15:35:59 +0000

    complete schema upload feature

--- app/Http/Controllers/Backend/QueryLogs/QueryLogsController.php
+++ app/Http/Controllers/Backend/QueryLogs/QueryLogsController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Backend\QueryLogs;
 
 use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use OpenAI;
 
 class QueryLogsController extends Controller
 {
@@ -44,5 +47,38 @@ class QueryLogsController extends Controller
         } catch (Exception $ex) {
             Log::info($ex);
         }
+    }
+
+    public function callOpenAIChatAPI(Request $request)
+    {
+        $schema = $request->schema;
+        $userText = $request->userText;
+        $language = $request->language;
+        $apiUrl = 'https://api.openai.com/v1/chat/completions';
+        $apiKey = config('services.open_ai.secret');
+        $client = new Client();
+
+        $response = $client->post($apiUrl, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $apiKey,
+            ],
+            'json' => [
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    ['role' => 'system', 'content' => 'You are a MySql database Engineer: <br>' . $schema ],
+                    ['role' => 'user', 'content' => 'Generate ' . $language . ' query without explanation: ' . $userText],
+                ],
+                'max_tokens' => 600,
+                "temperature" => 0.7,
+                "top_p" => 1,
+            ],
+        ]);
+
+        $responseData = json_decode($response->getBody(), true);
+
+        $result = $responseData['choices'][0]['message']['content'];
+
+        return response()->json($result);
     }
 }
